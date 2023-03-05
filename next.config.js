2cf722fa5d31ccf20f52f17831ee38ef7e0dcd89
Shubham Berkar <Sberkar12@gmail.com>
2023-03-05 22:45:59 +0530

    jsjfj

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pixabay.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
